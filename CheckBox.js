5344e2419f3b41620e6d67c6a73719a36e40d2cb
Peter Jekel <peter@thejekels.com>
2012-03-19 16:45:02 -0400

    Changed checked state cycle

--- CheckBox.js
+++ CheckBox.js
@@ -26,7 +26,7 @@ define([
   "dijit/form/CheckBox",
   "dojo/_base/declare",
   "dojo/dom-attr"
-], function(CheckBox, declare, domAttr) {
+], function( CheckBox, declare, domAttr ) {
 
   return declare( [CheckBox], {
     // baseClass: [protected] String
@@ -40,8 +40,26 @@ define([
     //    multiState is true the 'checked' attr can be either: 'mixed',
     //    true or false otherwise 'checked' can only be true or false.
     multiState: true,
+
+    _getCheckedAttr: function() {
+      return this.checked;
+    },
     
-    _setValueAttr: function(/*String or Boolean*/ newValue, /*Boolean*/ priorityChange){
+    _setCheckedAttr: function( /*Boolean | String*/ checked, /*Boolean?*/ priorityChange ) {
+      // summary
+      //    Set the new checked state of the checkbox.
+      // description
+      //    Set the new checked state of the checkbox.
+      //  state:
+      //    New state which is either true, false or 'mixed'
+      var newState = (this.multiState ? checked : (checked ? true : false));
+
+      this._set("checked", newState );      /* Fast track set() procedure */
+      domAttr.set(this.focusNode || this.domNode, "checked", newState );
+      (this.focusNode || this.domNode).setAttribute("aria-checked", newState );
+    },
+
+    _setValueAttr: function(/*String or Boolean*/ newValue, /*Boolean?*/ priorityChange){
       // summary:
       //    Handler for value= attribute to constructor, Overwrites the
       //    default '_setValueAttr' method as we will handle the Checkbox
@@ -49,26 +67,63 @@ define([
       // description:
       //    If passed a string, changes the value attribute of the CheckBox
       //    (the one specified as "value" when the CheckBox was constructed).
-
+      //
+      //    NOTE: Changing the checkbox value DOES NOT change the checked state.
+      
       if(typeof newValue == "string"){
         this.value = newValue;
         domAttr.set(this.focusNode, 'value', newValue);
+        this._handleOnChange(newValue, priorityChange);
       }
     },
 
-    _setCheckedState: function( /*Boolean | String*/ checked ) {
-      // summary
-      //    Set the new checked state of the checkbox.
-      // description
-      //    Set the new checked state of the checkbox and update the state
-      //    classes accordingly. 
-      //  state:
-      //    New state which is either true, false or 'mixed'
-
-      this.set( "checked", checked );
-      this._setStateClass();
+    _onClick: function( /*Event*/ evt ) {
+      // summary:
+      //    Process a click event on the checkbox.
+      // description:
+      //    Process a click event on the checkbox. If the checkbox is in a mixed
+      //    state it will change to checked. Any other state will just toggle the
+      //    current checkbox state.
+      //
+      //    NOTE: A click event will never change the state to mixed.
+      
+      if( this._toggleChecked() ) {
+        return this.onClick(evt);
+      }
+      event.stop(evt);
     },
-    
+
+    _onKeyPress: function(/*Event*/ evt ){
+      // summary:
+      //    Toggle the checkbox state when the user pressed the spacebar.
+      // description:
+      //    Toggle the checkbox state when the user pressed the spacebar.
+      //    The spacebar is only processed if the widget that has focus is
+      //    a tree node and has a checkbox.
+      //
+      if( !evt.altKey ) {
+        if( (typeof evt.charOrCode == "string") && (evt.charOrCode == " ") ) {
+          if( this._toggleChecked() ) {
+            return onKeyPress( evt );
+          }
+          event.stop(evt);
+        }
+      }
+    },
+
+    _toggleChecked: function() {
+      // summary:
+      //    Toggle the current checkbox state. 
+      //
+      if(!this.readOnly && !this.disabled){
+        var curState = this.get( "checked" );
+        this._setCheckedAttr( (curState == 'mixed' ? true : !curState ), null );      
+        return true;
+      }
+      return false;
+    }
+
   });  /* end declare() */
+
 
 });  /* end define() */
