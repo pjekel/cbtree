8322ea4e0c60036b3c5d49c210b48c68bcb05328
Peter Jekel <peter@thejekels.com>
2013-02-27 05:58:23 -0500

    Updated profile to include new modules

--- cbtree.profile.js
+++ cbtree.profile.js
@@ -2,21 +2,25 @@ var testResourceRe = /^cbtree\/tests\//;
 var excludePath = [ /^cbtree\/((.*)?\/)?tests\//,
 									  /^cbtree\/((.*)?\/)?demos\//,
 									  /^cbtree\/((.*)?\/)?json\//,
-									  /^cbtree\/((.*)?\/)?csv\//,
-									  /^cbtree\/((.*)?\/)?documentation\//
+									  /^cbtree\/((.*)?\/)?csv\//
 									 ];
 var copyOnly = function(filename, mid) {
 	var list = {
 		"cbtree/cbtree.profile":1,
-		"cbtree/cbtree.build":1,
 		"cbtree/package.json":1
 	};
-	return (mid in list) || (/^cbtree\/themes\//.test(mid) && !/\.css$/.test(filename)) || /(png|jpg|jpeg|gif|tiff)$/.test(filename);
+	return (mid in list) || 
+		(/^cbtree\/themes\//.test(mid) && !/\.css$/.test(filename)) || 
+		/(png|jpg|jpeg|gif|tiff)$/.test(filename);
 };
+
+// The following profile defines all dojo, dijit and CBTree modules required
+// to run all demos included in the CheckBox Tree package.
 
 var profile = {
 	releaseDir: "../release",
-	basePath : "..",
+	releaseName: "cbtree",
+	basePath: "..",
 	action: "release",
 	cssOptimize: "comments",
 	optimize: "closure",
@@ -24,44 +28,80 @@ var profile = {
 	selectorEngine: "acme",
 	mini: true,
 
+	staticHasFeatures: {
+		"config-deferredInstrumentation": 0
+	},
+
+	packages:[
+		{name:"dojo", location: "dojo"},
+		{name:"dijit", location: "dijit"},
+		{name:"cbtree", location: "cbtree"}	
+	],
+
 	layers: {
 		"dojo/dojo": {
-				include: [
-					"dojo/_base/array",
-					"dojo/dojo",
-					"dojo/data/ItemFileWriteStore",
-					"dojo/dom",
-					"dojo/domReady",
-					"dojo/i18n",
-					"dojo/ready"
-				],
-				customBase: true,
-				boot: true
+			include: [
+				"dojo/dojo",
+				"dojo/data/ItemFileWriteStore",		// Remove with 2.0
+				"dojo/dom",
+				"dojo/domReady",
+				"dojo/dnd/Manager",
+				"dojo/fx/Toggler",
+				"dojo/i18n",
+				"dojo/main",
+				"dojo/ready",
+				"dojo/store/Memory",
+				"dojo/store/Observable"
+			],
+			customBase: true,
+			boot: true
 		},
-		"cbtree/main": {
-				include: [
-					// Legacy dojo/data store models (remove with dojo 2.0)
-					"cbtree/models/FileStoreModel",
-					"cbtree/models/ForestStoreModel",
-					"cbtree/models/TreeStoreModel",
-					"cbtree/models/StoreModel-API",
-					"cbtree/data/FileStore",
-					// New dojo/store & cbtree/store models
-					"cbtree/model/FileStoreModel",
-					"cbtree/model/ForestStoreModel",
-					"cbtree/model/StoreModel-API",
-					"cbtree/model/TreeStoreModel",
-					// New cbtree/store stores & wrappers
-					"cbtree/store/Eventable",
-					"cbtree/store/FileStore",
-					"cbtree/store/Hierarchy",
-					"cbtree/store/Memory",
-					"cbtree/store/ObjectStore",
-					// cbtree
-					"cbtree/CheckBox",
-					"cbtree/TreeStyling",
-					"cbtree/Tree"
-				]
+
+		"dijit/dijit": {
+			include: [
+				"dijit/dijit",
+				"dijit/form/ComboButton",
+				"dijit/form/DropDownButton",
+				"dijit/form/RadioButton",
+				"dijit/tree/dndSource"
+			]
+		},
+
+		"cbtree/cbtree": {
+			include: [
+				// Legacy dojo/data store models (remove with dojo 2.0)
+				"cbtree/models/FileStoreModel",
+				"cbtree/models/ForestStoreModel",
+				"cbtree/models/TreeStoreModel",
+				"cbtree/models/StoreModel-API",
+				"cbtree/data/FileStore",
+				// New dojo/store & cbtree/store models
+				"cbtree/model/FileStoreModel",
+				"cbtree/model/ForestStoreModel",
+				"cbtree/model/StoreModel-EXT",
+				"cbtree/model/TreeStoreModel",
+				// New cbtree/store stores & wrappers
+				"cbtree/store/Eventable",
+				"cbtree/store/FileStore",
+				"cbtree/store/Hierarchy",
+				"cbtree/store/Memory",
+				"cbtree/store/Natural",
+				"cbtree/store/ObjectStore",
+				"cbtree/store/_CORS",
+				// cbtree/store/handlers
+				"cbtree/store/handlers/arcGisHandler",
+				"cbtree/store/handlers/csvHandler",
+				"cbtree/store/handlers/ifrsHandler",
+				// cbtree
+				"cbtree/CheckBox",
+				"cbtree/Tree",
+				// extensions
+				"cbtree/extensions/TreeStyling",
+				// util
+				"cbtree/util/QueryEngine",
+				// misc
+				"cbtree/errors/createError",
+			]
 		}
 	},
 
