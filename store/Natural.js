21625c01445f472ce968ad405f90eb7def56328b
Peter Jekel <peter@thejekels.com>
2013-03-02 18:19:27 -0500

    Moved function to Ancestry extension

--- store/Natural.js
+++ store/Natural.js
@@ -93,31 +93,6 @@ define(["dojo/_base/declare",
 			return this.inherited(arguments);
 		},
 
-		//=========================================================================
-		// Public methods
-
-		isBefore: function (/*Object|Id*/ itemA, /*Object|Id*/ itemB) {
-			// summary:
-			//		Evaluate if an item appears before another item in the natural
-			//		order of store objects.
-			// itemA:
-			// itemB:
-			// returns:
-			//		Boolean true if itemA appears before itemB otherwise false.
-			// tag:
-			//		Public
-			var objA = this._anyToObject(itemA);
-			var objB = this._anyToObject(itemB);
-
-			if (objA && objB) {
-				var indexA = this._data.indexOf( objA );
-				var indexB = this._data.indexOf( objB );
-
-				return (indexA < indexB);
-			}
-			return false;
-		},
-
 		toString: function () {
 			return "[object NaturalStore]";
 		}
