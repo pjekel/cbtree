09632b1e368aa759fff7364be7b0c3e1904b52b3
Peter Jekel <peter@thejekels.com>
2013-10-28 17:34:52 -0400

    Removed trailing comma dependency list

--- model/StoreModel-EXT.js
+++ model/StoreModel-EXT.js
@@ -9,23 +9,24 @@
 //	3 - The Academic Free License		(http://trac.dojotoolkit.org/browser/dojo/trunk/LICENSE#L43)
 //
 define(["module",
-				"dojo/_base/lang",
-				"dojo/when",
-				"./_base/BaseStoreModel",
-				"./_base/Parents",
-				"../errors/createError!../errors/CBTErrors.json"
-			 ], function (module, lang, when, BaseStoreModel, Parents, createError) {
+		"dojo/_base/lang",
+		"dojo/when",
+		"./_base/BaseStoreModel",
+		"./_base/Parents",
+		"../errors/createError!../errors/CBTErrors.json",
+		"../util/QueryEngine"
+	 ], function (module, lang, when, BaseStoreModel, Parents, createError, QueryEngine) {
 
 	// module:
 	//		cbtree/model/StoreModel-EXT
 	// summary:
 	//		This module extends the cbtree/model/_base/BaseStoreModel API with an
-	//		additional set of methods commenly used to query and/or modify store 
+	//		additional set of methods commonly used to query and/or modify store
 	//		objects checked state.
 
-	var CBTError = createError( module.id );		// Create the CBTError type.
+	var CBTError = createError(module.id);		// Create the CBTError type.
 
-	function isObject( object ) {
+	function isObject(object) {
 		// summary:
 		//		Returns true if an object is a key:value pairs object.
 		return (Object.prototype.toString.call(object) == "[object Object]");
@@ -36,25 +37,24 @@ define(["module",
 		// =======================================================================
 		// Private Methods related to checked states
 
-		_checkOrUncheck: function (/*String|Object*/ query, /*Boolean*/ newState, /*Callback*/ onComplete,
-																/*thisArg*/ scope) {
+		_checkOrUncheck: function (query, newState, onComplete, scope) {
 			// summary:
 			//		Check or uncheck the checked state of all store items that match the
 			//		query and have a checked state.
 			//		This method is called by either the public methods check() or uncheck()
 			//		providing an easy way to programmatically alter the checked state of a
 			//		set of store items associated with the tree nodes.
-			// query:
+			// query: String|Object
 			//		A query object or string.	 If query is a string the idProperty attribute
 			//		of the store is used as the query attribute and the query string assigned
 			//		as the associated value.
-			// newState:
+			// newState: Boolean
 			//		New state to be applied to the store items.
-			// onComplete:
+			// onComplete: Callback
 			//		If an onComplete callback function is provided, the callback function
 			//		will be called just once, after the last storeItem has been updated as:
 			//		onComplete(matches, updates).
-			// scope:
+			// scope: Object
 			//		If a scope object is provided, the function onComplete will be invoked
 			//		in the context of the scope object. In the body of the callback function,
 			//		the value of the "this" keyword will be the scope object. If no scope is
@@ -63,8 +63,8 @@ define(["module",
 			//		private
 
 			var storeOnly = this.checkedStrict ? true : false,
-					matches   = 0,
-					updates   = 0;
+				matches   = 0,
+				updates   = 0;
 
 			this.fetchItemsWithChecked(query, function (storeItems) {
 				storeItems.forEach(function (storeItem) {
@@ -73,9 +73,9 @@ define(["module",
 						updates += 1;
 					}
 					matches += 1;
-				}, this)
+				}, this);
 				if (onComplete) {
-					onComplete.call((scope ? scope : this), matches, updates);
+					onComplete.call((scope || this), matches, updates);
 				}
 			}, this, storeOnly);
 		},
@@ -83,19 +83,19 @@ define(["module",
 		// =======================================================================
 		// Inspecting and validating items
 
-		fetchItem: function (/*String|Object*/ args, /*Callback?*/ onComplete,/*thisArg?*/ scope) {
+		fetchItem: function (args, onComplete, scope) {
 			// summary:
 			//		Get the store item that matches args. Parameter args is either an
 			//		object or a string.
-			// args:
+			// args: String|Object
 			//		An object or string used to query the store. If args is a string its
 			//		value is assigned to the store idProperty in the query.
-			//	onComplete:
+			//	onComplete: Callback?
 			//		User specified callback method which is called on completion with the
 			//		first store item that matched the query argument. Method onComplete()
 			//		is called as: onComplete(storeItem) in the context of scope if scope
 			//		is specified otherwise in the active context (this).
-			//	scope:
+			//	scope: Object?
 			//		If a scope object is provided, the function onComplete will be invoked
 			//		in the context of the scope object. In the body of the callback function,
 			//		the value of the "this" keyword will be the scope object. If no scope
@@ -104,7 +104,8 @@ define(["module",
 			//		public
 
 			var idQuery = this._anyToQuery(args, null);
-			var scope   = scope || this;
+
+			scope = scope || this;
 
 			if (idQuery && onComplete) {
 				when(this.store.query(idQuery), function (queryResult) {
@@ -114,26 +115,25 @@ define(["module",
 			}
 		},
 
-		fetchItemsWithChecked: function (/*String|Object*/ query, /*Callback?*/ onComplete, /*thisArg?*/ scope,
-																			/*Boolean?*/ storeOnly) {
+		fetchItemsWithChecked: function (query, onComplete, scope, storeOnly) {
 			// summary:
 			//		Get the list of store items that match the query and have a checked
 			//		state (a checkedAttr property).
-			//	query:
+			//	query: String|Object
 			//		A query object or string. If query is a string the identifier attribute
 			//		of the store is used as the query attribute and the string assigned as
 			//		the associated value.
-			//	onComplete:
+			//	onComplete: Callback?
 			//		 User specified callback method which is called on completion with an
 			//		array of store items that matched the query argument. Method onComplete
 			//		is called as: onComplete(storeItems) in the context of scope if scope
 			//		is specified otherwise in the active context (this).
-			//	scope:
+			//	scope: Object?
 			//		If a scope object is provided, the function onComplete will be invoked
 			//		in the context of the scope object. In the body of the callback function,
 			//		the value of the "this" object will be the scope. If no scope object is
 			//		provided, onComplete will be called in the context of tree.model.
-			// storeOnly:
+			// storeOnly: Boolean?
 			//		Indicates if the fetch operation should be limited to the in-memory cache
 			//		only. Some stores may fetch data from a back-end server when performing a
 			//		deep search. However, when querying attributes, some attributes may only
@@ -142,14 +142,15 @@ define(["module",
 			// tag:
 			//		public
 
-			var storeQuery = this._anyToQuery( query, null );
+			var storeQuery = this._anyToQuery(query, null);
 			var storeItems = [];
 			var cacheOnly  = (storeOnly !== undefined) ? !!storeOnly : true;
-			var scope      = scope || this;
 			var self       = this;
 
+			scope = scope || this;
+
 			if (isObject(storeQuery)) {
-				when( this.store.query(storeQuery, {cacheOnly: cacheOnly}), function (items) {
+				when(this.store.query(storeQuery, {cacheOnly: cacheOnly}), function (items) {
 					items.forEach(function (item) {
 						if (self.checkedAttr in item) {
 							storeItems.push(item);
@@ -168,25 +169,25 @@ define(["module",
 					}
 				}, this.onError);
 			} else {
-				throw new CBTError( "InvalidObject", "fetchItemsWithChecked", "query must be of type object");
+				throw new CBTError("InvalidObject", "fetchItemsWithChecked", "query must be of type object");
 			}
 		},
 
-		isRootItem: function (/*AnyType*/ something) {
+		isRootItem: function (something) {
 			// summary:
 			//		Returns true if 'something' is a child of the root otherwise false.
-			// item:
+			// something: any
 			//		A valid dojo/store item.
 			// tag:
 			//		public
 
-			return this.isChildOf( something, this.root );
+			return this.isChildOf(something, this.root);
 		},
 
 		// =======================================================================
 		// Write interface
 
-		check: function (/*Object|String*/ query, /*Callback*/ onComplete, /*thisArg*/ scope, /*Boolean?*/ storeOnly) {
+		check: function (query, onComplete, scope, storeOnly) {
 			// summary:
 			//		Check all store items that match the query and have a checked state.
 			// description:
@@ -200,7 +201,7 @@ define(["module",
 			this._checkOrUncheck(query, true, onComplete, scope, storeOnly);
 		},
 
-		uncheck: function (/*Object|String*/ query, /*Callback*/ onComplete, /*thisArg*/ scope, /*Boolean?*/ storeOnly) {
+		uncheck: function (query, onComplete, scope, storeOnly) {
 			// summary:
 			//		Uncheck all store items that match the query and have a checked state.
 			// description:
@@ -214,14 +215,14 @@ define(["module",
 			this._checkOrUncheck(query, false, onComplete, scope, storeOnly);
 		},
 
-		_changeParents: function (/*Object*/ storeItem, /*Object*/ parent, /*String*/ operation) {
+		_changeParents: function (storeItem, parent, operation) {
 			// summary:
 			//		Add a parent to or remove a parent from a store object.
-			// storeItem:
+			// storeItem: Object
 			//		A valid (existing) Store object
-			// parent:
+			// parent: Object
 			//		Parent object.
-			// operation:
+			// operation: String
 			//		"attach" | "detach"
 			// tag:
 			//		Private
@@ -233,7 +234,7 @@ define(["module",
 				var isRoot   = (this._forest && this.root.id == itemId);
 
 				if (itemId && parentId && !isRoot) {
-					when( this.store.get(itemId), function (item) {
+					when(this.store.get(itemId), function (item) {
 						if (item) {
 							var isRootChild = self.isChildOf(item, self.root);
 							var parentIds   = new Parents(item, self.parentProperty);
@@ -241,12 +242,12 @@ define(["module",
 
 							if (forestRoot) {
 								if (!parentIds.multiple) {
-										item[self.parentProperty] = undefined;
-										this.store.put(item);
+									item[self.parentProperty] = undefined;
+									this.store.put(item);
 								}
 								if ((!isRootChild && operation == "attach") || (isRootChild && operation == "detach")) {
-									var children = self._updateChildrenCache(operation, parent, storeItem );
-									when( children, function () {
+									var children = self._updateChildrenCache(operation, parent, storeItem);
+									when(children, function () {
 										self.onRootChange(storeItem, operation);
 										self._childrenChanged(self.root);
 									});
@@ -256,34 +257,34 @@ define(["module",
 									item[self.parentProperty] = parentIds.toValue();
 									itemId = self.store.put(item);
 									if (!self._monitored) {
-										when( itemId, function () {
+										when(itemId, function () {
 											self._childrenChanged(parent);
 										});
 									}
 								}
 							}
 						} else {
-							throw new CBTError( "InvalidObject", "addParent", "Item ["+itemId+"] is not a valid store item");
+							throw new CBTError("InvalidObject", "addParent", "Item [" + itemId + "] is not a valid store item");
 						}
 					});
 				}
 			}
 		},
 
-		addParent: function (/*Object*/ storeItem, /*Object|String*/ parent) {
+		addParent: function (storeItem, parent) {
 			// summary:
 			//		Add a parent to a store item.
-			// storeItem:
-			// parent:
+			// storeItem: Object
+			// parent: Object | String
 			// tag:
 			//		Public
 			if (typeof parent == "string") {
 				var self = this;
-				when (this.store.get(parent), function(parent) {
-					self._changeParents( storeItem, parent, "attach" );
+				when(this.store.get(parent), function (parent) {
+					self._changeParents(storeItem, parent, "attach");
 				});
 			} else {
-				this._changeParents( storeItem, parent, "attach" );
+				this._changeParents(storeItem, parent, "attach");
 			}
 		},
 
@@ -296,25 +297,25 @@ define(["module",
 			//		Public
 			if (typeof parent == "string") {
 				var self = this;
-				when (this.store.get(parent), function(parent) {
-					self._changeParents( storeItem, parent, "detach" );
+				when(this.store.get(parent), function (parent) {
+					self._changeParents(storeItem, parent, "detach");
 				});
 			} else {
-				this._changeParents( storeItem, parent, "detach" );
+				this._changeParents(storeItem, parent, "detach");
 			}
 		},
 
 		// =======================================================================
 		// Misc Private Methods
 
-		_anyToQuery: function (/*String|Object*/ args, /*String?*/ attribute) {
+		_anyToQuery: function (args, attribute) {
 			// summary:
 			//		Compose a query object.
-			// args:
+			// args: String|Object
 			//		 Query object, if args is a string it value is assigned to the store
 			//		identifier property (idProperty) in the query object.
-			// attribute:
-			//		Optional attribute name.	If specified, the attribute in args to be
+			// attribute: String?
+			//		Optional attribute name. If specified, the attribute in args to be
 			//		used as its identifier. If an external item is dropped on the tree,
 			//		the new item may not have the same identifier property as all store
 			//		items do.
@@ -327,28 +328,25 @@ define(["module",
 
 			if (identAttr) {
 				var objAttr = attribute || identAttr,
-						query   = {};
+					query   = {};
 				if (typeof args == "string") {
 					query[identAttr] = args;
-					return query;
-				}
-				if (args && isObject(args)) {
-					lang.mixin( query, args );
+				} else if (args && isObject(args)) {
+					lang.mixin(query, args);
 					if (objAttr in args) {
-						query[identAttr] = args[objAttr]
+						query[identAttr] = args[objAttr];
 					}
-					return query;
 				} else {
 					query[identAttr] = /.*/;
-					return query;
 				}
+				return query;
 			}
 			return null;
 		}
 
 	};	/* end ModelExtension {} */
 
-	BaseStoreModel.extend( ModelExtension );
+	BaseStoreModel.extend(ModelExtension);
 	return true;
-	
+
 });	/* end define() */
